5947eaa3dbfdb0e03b7dddad3b36247c90d6c0b8
Chester Frazier <82249228+cwfrazier1@users.noreply.github.com>
2021-12-28 10:22:06 -0600

    Create index.php
    version 1

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+    function generate($type = 'password', $length = 14)
+    {
+        $time = time();
+
+        $characters = '';
+
+        if ($type == 'password')
+            $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*(),./<>?;:{}|[]\-=_+~';
+        else
+            $characters = '0123456789abcdefghijklmnopqrstuvwxyz';
+
+        $charactersLength = strlen($characters);
+        $randomString = '';
+
+        for ($i = 0; $i < $length; $i++)
+        {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+
+        $randomString = $time . $randomString;
+
+        return $randomString;
+    }
+
+
+    $emailAddress = generate('sensative', '20')."@cwfrazier.com";
+    $password = generate('password', 20);
+?>
+
+<html>
+    <head>
+        <title>New Password</title>
+
+    </head>
+    <body>
+    <form method="POST" action="index.php">
+<?php
+    echo "
+        <table>
+            <tr><td>Email:</td><td><div id=\"email\">$emailAddress</div></td></tr>
+            <tr><td>Password:</td><td><div id=\"pass\">$password</div></td></tr>
+        </table>
+";
+
+?>
+
+    </form>
+    </body>
+</html>
